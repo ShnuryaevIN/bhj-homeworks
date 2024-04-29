f25e72bfb8d08f8d4bd325525dc7be76bc98fcf3
Shnuryaev I.N <shnuryaevin@gmail.com>
2024-04-29 19:31:07 +0500

    Задание 1 выполнено

---
+++ js-features/countdown/task.js
@@ -0,0 +1,14 @@
+let timer = document.querySelector("#timer");
+let count = timer.textContent;
+
+function updateCounter() {
+    count --;
+    timer.textContent = count;
+
+    if(count == 0) {
+        clearInterval(timerID);
+        alert("Вы победили в конкурсе!")
+    }
+}
+
+const timerID = setInterval(updateCounter, 1000);
